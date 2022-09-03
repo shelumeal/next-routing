f14f84cb0cbf0a190a639d4a09092d4725a1e7a1
Shelumiel <sssharon7@gmail.com>
2022-09-03 16:35:14 +0800

    Add Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const rounter = useRouter();
+
+  const handleClick = () => {
+    rounter.push("/product");
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -10,6 +17,7 @@ function Home() {
       <Link href="/product">
         <a>Product</a>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
