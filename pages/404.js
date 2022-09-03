b2953a7cd13150f20ea0ae18821d4ad0e145df08
Shelumiel <sssharon7@gmail.com>
2022-09-03 17:28:03 +0800

    Add custom 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div>
+      <h1>404 Page with all the custome styling.</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
